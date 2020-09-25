d27b25cc22fc9baa45f7af614bc8ed6184ac773e
Alex Angelo <alexangelo87@gmail.com>
2020-09-24 21:19:54 -0300

    fix type

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import express, {Request, Response, Router, Express} from 'express';
+import express, {Application} from 'express';
 import jogadoresRouter from './routes/jogadoresRouter';
 import bodyParser from 'body-parser';
-const app: Express  = express();
+const app: Application  = express();
 
 app.use(bodyParser.json({limit: '5mb'}))
 app.use('/jogadores', jogadoresRouter )
